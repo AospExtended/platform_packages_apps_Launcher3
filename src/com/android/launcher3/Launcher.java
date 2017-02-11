dd98074a1e38ffd65a54e743aea048c0824d54b5
Thecrazyskull <anaskarbila@gmail.com>
2017-02-11 08:14:08 -0500

    Launcher3: support google now tab
    Change-Id: I6efc1d5186e0f1bd4fa78ae7231999b984ce7222

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -330,6 +330,7 @@ public class Launcher extends BaseActivity
     private boolean mShowPredictiveApps;
 
     private Context mContext;
+    private LauncherTab mLauncherTab;
 
     @Thunk void setOrientation() {
         if (mRotationEnabled) {
@@ -449,6 +450,8 @@ public class Launcher extends BaseActivity
         // For handling default keys
         mDefaultKeySsb = new SpannableStringBuilder();
         Selection.setSelection(mDefaultKeySsb, 0);
+
+        mLauncherTab = new LauncherTab(this);
 
         mRotationEnabled = getResources().getBoolean(R.bool.allow_rotation);
         // In case we are on a device with locked rotation, we should look at preferences to check
@@ -1079,6 +1082,9 @@ public class Launcher extends BaseActivity
             mAllAppsController.showDiscoveryBounce();
         }
         mIsResumeFromActionScreenOff = false;
+
+        mLauncherTab.getClient().onResume();
+
         if (mLauncherCallbacks != null) {
             mLauncherCallbacks.onResume();
         }
@@ -1101,6 +1107,8 @@ public class Launcher extends BaseActivity
         if (mWorkspace.getCustomContentCallbacks() != null) {
             mWorkspace.getCustomContentCallbacks().onHide();
         }
+
+        mLauncherTab.getClient().onPause();
 
         if (mLauncherCallbacks != null) {
             mLauncherCallbacks.onPause();
@@ -1616,6 +1624,8 @@ public class Launcher extends BaseActivity
         FirstFrameAnimatorHelper.initializeDrawListener(getWindow().getDecorView());
         mAttached = true;
 
+        mLauncherTab.getClient().onAttachedToWindow();
+
         if (mLauncherCallbacks != null) {
             mLauncherCallbacks.onAttachedToWindow();
         }
@@ -1628,6 +1638,8 @@ public class Launcher extends BaseActivity
             unregisterReceiver(mReceiver);
             mAttached = false;
         }
+
+        mLauncherTab.getClient().onDetachedFromWindow();
 
         if (mLauncherCallbacks != null) {
             mLauncherCallbacks.onDetachedFromWindow();
@@ -1792,6 +1804,8 @@ public class Launcher extends BaseActivity
                 mWidgetsView.scrollToTop();
             }
 
+            mLauncherTab.getClient().hideOverlay(true);
+
             if (mLauncherCallbacks != null) {
                 mLauncherCallbacks.onHomeIntent();
             }
@@ -1896,6 +1910,8 @@ public class Launcher extends BaseActivity
                 .removeAccessibilityStateChangeListener(this);
 
         LauncherAnimUtils.onDestroyActivity();
+
+        mLauncherTab.getClient().onDestroy();
 
         if (mLauncherCallbacks != null) {
             mLauncherCallbacks.onDestroy();
