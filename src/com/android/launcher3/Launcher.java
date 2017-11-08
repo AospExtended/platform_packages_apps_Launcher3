64875cb4a5a72b03dc9d50e64e10d31749b52c00
maxwen <max.weninger@gmail.com>
2017-11-08 14:02:27 +0100

    Launcher3: add config to show current day in top widget
    Change-Id: I7c612aeb3f9eb03bce4e9e376bc010a445ce6d41

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -4239,7 +4239,8 @@ public class Launcher extends BaseActivity
                 }
             }
             if (Utilities.SHOW_ALL_DAY_EVENTS_PREFERENCE_KEY.equals(key) ||
-                    Utilities.SHOW_EVENTS_PERIOD_PREFERENCE_KEY.equals(key)) {
+                    Utilities.SHOW_EVENTS_PERIOD_PREFERENCE_KEY.equals(key) ||
+                    Utilities.SHOW_TODAY_PREFERENCE_KEY.equals(key)) {
                 if (mCalendarView != null) {
                     mCalendarView.updateSettings();
                 }
