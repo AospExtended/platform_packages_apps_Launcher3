c23d4de29e2c6cb9ff8f2ac3868c0712563d867b
Anas Karbila <anaskarbila@aospa.co>
2018-02-18 16:00:54 +0100

    WorkSpace: add fade out animation to items when switching page
    * Idea taken from OnePlus launcher
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -1383,8 +1383,21 @@ public class Workspace extends PagedView
         }
 
         updatePageAlphaValues();
+        updatePageContentAlpha();
         updateStateForCustomContent();
         enableHwLayersOnVisiblePages();
+    }
+
+    private void updatePageContentAlpha() {
+        int screenCenter = getScrollX() + getViewportWidth() / 2;
+        for (int i = numCustomPages(); i < getChildCount(); i++) {
+            CellLayout child = (CellLayout) getChildAt(i);
+            if (child != null) {
+                float scrollProgress = getScrollProgress(screenCenter, child, i);
+                float alpha = 1 - (isInOverviewMode() ? 0 : Math.abs(scrollProgress));
+                child.setAlpha(alpha);
+            }
+        }
     }
 
     private void showPageIndicatorAtCurrentScroll() {
