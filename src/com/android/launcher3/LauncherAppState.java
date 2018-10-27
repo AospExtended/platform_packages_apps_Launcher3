5ad819126fe756f3ad2e727595cfcf22a9b684bf
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2018-10-27 19:53:36 +0000

    Settings restart watcher improvements
    Before this, the home button watcher and its receiver
    were started as soon as SettingsActivity was being created,
    listening for home button touches even if the user didn't change
    any option that required a restart.
    
    Now instead we start listening only if one of those options
    is changed, and we stop when we are done and the reset is
    triggered.
    
    Change-Id: I9aa209083ddc56ca0679a6d807670134cd21dc69
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/LauncherAppState.java
+++ src/com/android/launcher3/LauncherAppState.java
@@ -41,6 +41,7 @@ import com.android.launcher3.notification.NotificationListener;
 import com.android.launcher3.pm.InstallSessionHelper;
 import com.android.launcher3.pm.InstallSessionTracker;
 import com.android.launcher3.pm.UserCache;
+import com.android.launcher3.settings.HomeKeyWatcher;
 import com.android.launcher3.util.MainThreadInitializedObject;
 import com.android.launcher3.util.Preconditions;
 import com.android.launcher3.util.RunnableList;
@@ -67,6 +68,8 @@ public class LauncherAppState {
     private final DatabaseWidgetPreviewLoader mWidgetCache;
     private final InvariantDeviceProfile mInvariantDeviceProfile;
     private final RunnableList mOnTerminateCallback = new RunnableList();
+    private HomeKeyWatcher mHomeKeyListener = null;
+    private boolean mNeedsRestart;
 
     public static LauncherAppState getInstance(final Context context) {
         return INSTANCE.get(context);
@@ -148,6 +151,31 @@ public class LauncherAppState {
         if (areNotificationDotsEnabled) {
             NotificationListener.requestRebind(new ComponentName(
                     mContext, NotificationListener.class));
+        }
+
+        mHomeKeyListener = new HomeKeyWatcher(mContext);
+    }
+
+    public void setNeedsRestart() {
+        if (mNeedsRestart) {
+            // another pref change already called a restart
+            return;
+        }
+        mNeedsRestart = true;
+        mHomeKeyListener.startWatch();
+        mHomeKeyListener.setOnHomePressedListener(() -> {
+            mHomeKeyListener.stopWatch();
+            Utilities.restart(mContext);
+            // we're killing the whole process so no need to set mNeedsRestart to false again
+        });
+    }
+
+    public void checkIfRestartNeeded() {
+        // we destroyed Settings activity with the back button
+        // so we force a restart now if needed without waiting for home button press
+        if (mNeedsRestart) {
+            mHomeKeyListener.stopWatch();
+            Utilities.restart(mContext);
         }
     }
 
