d8db937f7ef705b97dba3a53c4c917f1c6903a91
ElDainosor <eldainosor@gmail.com>
2018-11-01 21:22:10 -0500

    AospLauncher: Set a toggle to show At A Glance
    Instead of hardcoding it on the FeatureFlags, let's make it
    toggleable as any other option would do
    
    Change-Id: I59893308efa357d25bdd3d53e31904e90c4d84ca
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -129,7 +129,7 @@ public class SettingsActivity extends Activity
                 c.getPackageManager().setComponentEnabledSetting(cn, newValue,
                         PackageManager.DONT_KILL_APP);
             }
-        } else if (Utilities.SHOW_WORKSPACE_GRADIENT.equals(key) || Utilities.SHOW_HOTSEAT_GRADIENT.equals(key)) {
+        } else if (Utilities.SHOW_WORKSPACE_GRADIENT.equals(key) || Utilities.SHOW_HOTSEAT_GRADIENT.equals(key) || Utilities.KEY_SHOW_SEARCHBAR.equals(key)) {
             LauncherAppState.getInstanceNoCreate().setNeedsRestart();
         }
     }
