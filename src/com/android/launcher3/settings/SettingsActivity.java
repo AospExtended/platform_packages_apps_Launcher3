c4f0e0ad7593cde0a517242b750f5ec40109437a
DennySPB <dennyspb@gmail.com>
2019-09-27 15:36:58 +0300

    Launcher3: Add restart action
    Change-Id: I562ef0baead993a0b1e2881a399ea97a98513f3f
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -137,7 +137,10 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { }
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (Utilities.KEY_KEY_DT_GESTURE.equals(key)) {
+                Utilities.restart(this);
+   }
 
     private boolean startPreference(String fragment, Bundle args, String key) {
         if (Utilities.ATLEAST_P && getSupportFragmentManager().isStateSaved()) {
