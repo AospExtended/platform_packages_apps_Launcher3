a283457f23d35297da6ee5301b0208582fcafef0
Pranav Vashi <neobuddy89@gmail.com>
2019-10-20 17:06:08 +0530

    Launcher3: Include manifests properly
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- Android.mk
+++ Android.mk
@@ -191,6 +191,10 @@ LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/quickstep/recents_ui_overrides/res
 LOCAL_PROGUARD_ENABLED := disabled
 
+LOCAL_FULL_LIBS_MANIFEST_FILES := \
+    $(LOCAL_PATH)/quickstep/AndroidManifest.xml \
+    $(LOCAL_PATH)/AndroidManifest.xml \
+    $(LOCAL_PATH)/AndroidManifest-common.xml
 
 LOCAL_MANIFEST_FILE := quickstep/AndroidManifest.xml
 include $(BUILD_STATIC_JAVA_LIBRARY)
@@ -222,6 +226,7 @@ LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/quickstep/recents_ui_overrides/res
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := \
+    $(LOCAL_PATH)/quickstep/AndroidManifest.xml \
     $(LOCAL_PATH)/AndroidManifest.xml \
     $(LOCAL_PATH)/AndroidManifest-common.xml
 
