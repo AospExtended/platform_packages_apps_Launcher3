6d8e576dea73b39b34fa1e9032e3b6f5e0b66801
Chris Crump <chris@cypheros.co>
2019-12-25 02:00:14 -0500

    AospLauncher: Add support for Google QuickSearchBox
    Moves QSB to the hotseat and adds support for basic Google search functionality. This iteration does not support extended features (ex. Google Doodle) but has plans to support these features later on
    
    Change-Id: Ia905b475e133c82814745e0cd037d07f7b41d1a9
    
    AospLauncher: Decrease QSB search task delay
    
    Change-Id: I2fc47e7cbeae257da1cdd6492c9b0da8d9686f3e

--- src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
+++ src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -155,7 +155,7 @@ public class AppsSearchContainerLayout extends ExtendedEditText
         mApps = appsView.getApps();
         mAppsView = appsView;
         mSearchBarController.initialize(
-                new DefaultAppSearchAlgorithm(mApps.getApps()), this, mLauncher, this);
+                new DefaultAppSearchAlgorithm(getContext(), mApps.getApps()), this, mLauncher, this);
     }
 
     @Override
