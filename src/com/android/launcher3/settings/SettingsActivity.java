cc18943235113bb8da0cf8d48da8a558844d4a54
Amir Zaidi <azaidi@live.nl>
2020-01-04 14:34:48 +0100

    AospLauncher: Replace top search bar with At A Glance
    AospLauncher: Rebind At A Glance when clicking default date widget
    
    At-A-Glance: Rename toggle accordingly
    
     * Also partially/fully revert e89dc99985c108b84ddb8958dc46f56393874ac7
    
    AEX edit -
    
     * Port to android 12
    
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I681c10808646583cc3c8231a55b7983213adc642
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -142,7 +142,7 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (Utilities.KEY_DT_GESTURE.equals(key)) {
                 LauncherAppState.getInstanceNoCreate().setNeedsRestart();
-        } else if (Utilities.KEY_SHOW_SEARCHBAR.equals(key)) {
+        } else if (Utilities.AT_A_GLANCE_SHOW.equals(key)) {
                 LauncherAppState.getInstanceNoCreate().setNeedsRestart();
         }
     }
