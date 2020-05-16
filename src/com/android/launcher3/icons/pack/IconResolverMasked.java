8c97f1a67910e72da2d2f0e238f7ba5ac950b3ff
Amir Zaidi <azaidi@live.nl>
2020-05-16 22:00:53 +0200

    Skip wrapping when creating masked icon pack icons
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: If812620ab8a4338dbea843eda270320c303bdf34
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/icons/pack/IconResolverMasked.java
+++ src/com/android/launcher3/icons/pack/IconResolverMasked.java
@@ -61,8 +61,13 @@ public class IconResolverMasked implements IconResolver {
         try {
             Resources res = pm.getResourcesForApplication(mPackInfo);
 
-            // Scale the bitmap using the icon pack scale.
+            // Re-render without scaling after creating the bitmap in the right dimensions.
             Bitmap iconBm = li.createScaledBitmapWithoutShadow(icon, 0);
+            mCanvas.setBitmap(iconBm);
+            icon.setBounds(0, 0, iconBm.getWidth(), iconBm.getHeight());
+            icon.draw(mCanvas);
+
+            // Scale the bitmap using the icon pack scale.
             scaleBitmap(iconBm, mData.scale);
 
             // Cut parts off using the mask image.
@@ -134,8 +139,12 @@ public class IconResolverMasked implements IconResolver {
     @SuppressLint("WrongConstant")
     private void backBitmap(Bitmap bitmap, Drawable back, LauncherIcons li) {
         if (back != null) {
-            back.setChangingConfigurations(back.getChangingConfigurations() | CONFIG_HINT_NO_WRAP);
-            Bitmap backBm = li.createScaledBitmapWithoutShadow(back, 0);
+            Bitmap backBm = Bitmap.createBitmap(
+                    bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+            mCanvas.setBitmap(backBm);
+            back.setBounds(0, 0, bitmap.getWidth(), bitmap.getHeight());
+            back.draw(mCanvas);
 
             mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OVER));
             mCanvas.setBitmap(bitmap);
