291c6766748b882a0e7f9698ffe7f3e1b0978b7b
Amir Zaidi <azaidi@live.nl>
2020-06-11 16:25:23 +0200

    Reprogram structure to load list preferences on background thread
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ibc6e2c8e01a446fd63eebb1c7f22107750ceed84

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -247,7 +247,8 @@ public class SettingsActivity extends FragmentActivity
 
                 case KEY_ICON_PACK:
                     ReloadingListPreference icons = (ReloadingListPreference) findPreference(KEY_ICON_PACK);
-                    icons.setOnReloadListener(new IconPackPrefSetter(mContext));
+                    icons.setValue(IconDatabase.getGlobal(mContext));
+                    icons.setOnReloadListener(IconPackPrefSetter::new);
                     icons.setOnPreferenceChangeListener((pref, val) -> {
                         IconDatabase.clearAll(mContext);
                         IconDatabase.setGlobal(mContext, (String) val);
