cf8fe9bd4c4839ae96af78f26f48f39455711d19
Pranav Vashi <neobuddy89@gmail.com>
2021-02-27 08:47:30 +0530

    Launcher3: Use custom seekbar for icon size
    idoybh:
    * Adapted to our settings.
    * Imported CustomSeekBarPreference from YASP
    * Adjusted interval and range sanely (OCD)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Icf62dd3b965d283bcc1d261692db07da9c8ec5fc

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -38,7 +38,6 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.preference.DropDownPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragmentCompat;
@@ -56,6 +55,7 @@ import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.model.WidgetsModel;
+import com.android.launcher3.settings.preference.CustomSeekBarPreference;
 import com.android.launcher3.uioverrides.plugins.PluginManagerWrapper;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
@@ -290,13 +290,11 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                     return true;
 
                 case Utilities.ICON_SIZE:
-                    final DropDownPreference iconSizes = (DropDownPreference) findPreference(Utilities.ICON_SIZE);
-                    iconSizes.setSummary(iconSizes.getEntry());
+                    final CustomSeekBarPreference iconSizes = (CustomSeekBarPreference)
+                            findPreference(Utilities.ICON_SIZE);
                     iconSizes.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
                         public boolean onPreferenceChange(Preference preference, Object newValue) {
-                            int index = iconSizes.findIndexOfValue((String) newValue);
-                            iconSizes.setSummary(iconSizes.getEntries()[index]);
-                            Utilities.restart(getActivity());
+                            LauncherAppState.getInstanceNoCreate().setNeedsRestart();
                             return true;
                         }
                     });
