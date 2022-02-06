341544a201ee7129340cc1915cccdee670dd6e45
Pranav Vashi <neobuddy89@gmail.com>
2022-02-07 01:50:09 +0530

    Launcher3: Enable lens icon for dock searchbar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I420e4267e03f340eeb3916a59e29e580948af2bd
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>

--- src/com/android/launcher3/qsb/QsbContainerView.java
+++ src/com/android/launcher3/qsb/QsbContainerView.java
@@ -47,6 +47,7 @@ import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.graphics.FragmentWithPreview;
+import com.android.launcher3.settings.SettingsActivity;
 import com.android.launcher3.widget.util.WidgetSizes;
 
 /**
@@ -74,6 +75,9 @@ public class QsbContainerView extends FrameLayout {
             ComponentName componentName = searchManager.getGlobalSearchActivity();
             if (componentName != null) {
                 providerPkg = searchManager.getGlobalSearchActivity().getPackageName();
+            }
+            if (providerPkg == null && SettingsActivity.LauncherSettingsFragment.isGSAEnabled(context)) {
+                providerPkg = Utilities.GSA_PACKAGE;
             }
         }
         return providerPkg;
